5ce150bf023f987a54398247f0fc3b8db9405e90
Michael Alexander <alexandermw@gmail.com>
2014-08-06 11:02:28 -0700

    updated to take requests

--- simple_server.rb
+++ simple_server.rb
@@ -1,9 +1,55 @@
 require 'socket'
 
-server = TCPServer.open(2000) #open method is the exact same as File.open. inherits from IO
-loop {
-  client = server.accept #instance method of TCPServer. waits for connection, returns TCPSocket representing that connection
-  client.puts(Time.now.ctime) #now when puts to that socket, client picks up on other side.
-  client.puts "Closing the connection. Bye!"
-  client.close
-}
+
+module SimpleServer
+
+class Server
+
+  def initialize
+    @server = TCPServer.open(2000) #open method is the exact same as File.open. inherits from IO
+  end
+
+  def run
+    loop do
+      client = @server.accept #instance method of TCPServer. waits for connection, returns TCPSocket representing that connection
+      
+      
+      while line = client.gets #gets requests from client
+        puts line.chomp
+        break if line =~ /^\s*$/
+        request = Request.new(line) # parses the line into a Request
+      end
+
+      client.puts(Time.now.ctime) #now when puts to that socket, client picks up on other side.
+      client.puts "Closing the connection. Bye!"
+      client.close
+    end
+  end
+  
+end
+
+#parses a string into the three parts of an HTML request
+class Request
+  attr_reader :method, :address, :version
+  def initialize(string)
+    if string ~= /^GET/
+      parts = string.split(" ") #an HTML GET is split by spaces
+      @method = parts[0] #first part is GET
+      @address = parts[1] #second part is the address
+      @version = parts[2] #third part is the HTML version
+    elsif string ~= /^POST/
+      @method = "POST"
+      #deal with rest of lines
+    end
+  end
+  
+  def post?
+    @method == "POST"
+  end
+  
+  def get?
+    @method == "GET"
+  end
+end
+
+end
